85355753c6db19835f5afd5c6edaa762a863dfb3
Maksim Mamaev <maksim.mamaev.12@gmail.com>
2022-06-11 12:19:58 +0300

    fix

--- pingscript.sh
+++ pingscript.sh
@@ -1,7 +1,9 @@
-#/bin/bash
+#!/bin/bash
 HTTPCODE=$(curl -o /dev/null -s -w "%{http_code}\n" $1)
 
 if [[ $HTTPCODE -eq 200 ]]
 then
-  echo "Success"
+    echo "Success"
+else
+    echo "Failure"
 fi
